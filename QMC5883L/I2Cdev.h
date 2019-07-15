21505953fb831b8b92c56a00a4f43628f1e22bf0
Ilor5 <48730197+Ilor5@users.noreply.github.com>
2019-07-15 22:24:36 +0300

    added coments to QMC5883L library

--- QMC5883L/I2Cdev.h
+++ QMC5883L/I2Cdev.h
@@ -37,14 +37,17 @@ THE SOFTWARE.
 #include <stdlib.h>
 #include <string.h>
 
-
+// TODO: include depending on chip family
+//#include "stm32f1xx_hal.h"
+//#include "stm32f2xx_hal.h"
+//#include "stm32f3xx_hal.h"
 #include "stm32l4xx_hal.h"
 
 typedef int bool;
 #define true 1
 #define false 0
 
-uint16_t I2Cdev_readTimeout;
+//uint16_t I2Cdev_readTimeout;
 
 // 1000ms default read timeout (modify with "I2Cdev::readTimeout = [ms];")
 #define I2CDEV_DEFAULT_READ_TIMEOUT     1000
--- QMC5883L/QMC5883L.cpp
+++ QMC5883L/QMC5883L.cpp
@@ -1,65 +1,64 @@
-/*
-  QMC5883L.cpp - QMC5883L library
-  Copyright (c) 2017 e-Gizmo Mechatronix Central
-  Rewritten by Amoree.  All right reserved.
-  July 10,2017
-
-  SET continuous measurement mode
-  OSR = 512
-  Full Scale Range = 8G(Gauss)
-  ODR = 200HZ
- 
-*/
-
+// Либа для магнитометра
 #include "QMC5883L.h"
 
-uint8_t address = QMC5883L_ADDR;
-
-void QMC5883L_setAddress(uint8_t addr)
-{
-  address = addr;
+/* Записать значение val в регистр */
+void QMC5883L_WriteReg(byte Reg, byte val) {
+	 I2Cdev_writeByte(QMC5883L_ADDR, Reg, val);
 }
 
-void QMC5883L_WriteReg(byte Reg, byte val)
-{
-  I2Cdev_writeByte(address, Reg, val);
+/* Инициализация магнитометра */
+void QMC5883L_init() {
+	QMC5883L_WriteReg(0x0B, 0x01);
+	QMC5883L_WriteReg(0x09, 0x1D);
 }
 
-void QMC5883L_init()
-{
-  QMC5883L_WriteReg(0x0B, 0x01);
-  //Define Set/Reset period
-  QMC5883L_setMode(Mode_Continuous, ODR_200Hz, RNG_8G, OSR_512);
-  QMC5883L_setMode()
+/* Сбросить софт (трогать не нужно)*/
+void QMC5883L_softReset() {
+	QMC5883L_WriteReg(0x0A, 0x80);
 }
 
-void QMC5883L_setMode(uint16_t mode, uint16_t odr, uint16_t rng, uint16_t osr)
-{
-  QMC5883L_WriteReg(0x09, mode | odr | rng | osr);
-  //Serial.println(mode|odr|rng|osr,HEX);
-}
+/* Считать значение с магнитометра в result */
+void QMC5883L_read(float *result) {
+	int regAddress = 0x00; //адрес регистра (не трогать)
+	byte buff[6];	//буфер для считывания данных
+	float dataMagnet[3];  // массив данных магнитометра
+	float rawDataMagnet[3];	// массив сырых данных магнитометра
+	I2Cdev_readBytes(QMC5883L_ADDR, regAddress, 6, buff, 0);
 
-void QMC5883L_softReset()
-{
-  QMC5883L_WriteReg(0x0A, 0x80);
-}
+	rawDataMagnet[0] = ((buff[1] << 8) + buff[0]); // x-direction
+	rawDataMagnet[1] = ((buff[3] << 8) + buff[2]); // y-direction
+	rawDataMagnet[2] = ((buff[5] << 8) + buff[4]); // z-direction
+	
+	for (int i = 0; i < 3; i++) {
+		if (rawDataMagnet[i] > 32768) {
+			rawDataMagnet[i] = rawDataMagnet[i] - 65536;		//проверка на переполнение
+		}
+	}
 
-void QMC5883L_read(uint32_t* result)
-{
-  // Wire.beginTransmission(address);
-  // Wire.write(0x00);
-  // Wire.endTransmission();
-  int regAddress = 0x00;
-  byte buff[6];
-  //Wire.requestFrom(address, 6);
-  // *x = Wire.read();                           //LSB  x
-  // *x |= Wire.read() << 8;                     //MSB  x
-  // *y = Wire.read();                           //LSB  z
-  // *y |= Wire.read() << 8;                     //MSB z
-  // *z = Wire.read();                           //LSB y
-  // *z |= Wire.read() << 8;                     //MSB y
-  I2Cdev_readByte(address, regAddress, buff, 0); //read the gyro data from the ITG3205
-  result[0] = ((buff[0] << 8) | buff[1]);        //x-direction
-  result[1] = ((buff[2] << 8) | buff[3]);        //y-direction
-  result[2] = ((buff[4] << 8) | buff[5]);        //z-direction
+	//заполнение массива данных
+	dataMagnet[0] = rawDataMagnet[0] / 30.0;
+	dataMagnet[1] = rawDataMagnet[1] / 30.0;
+	dataMagnet[2] = rawDataMagnet[2] / 30.0;
+	
+	//калибровочные параметры (выставить в зависимости от спутника)
+	float Bx, By, Bz;  
+	float a[3][3];
+	float b[3];
+	a[0][0] = 1.127500;
+	a[0][1] = -0.002647;
+	a[0][2] = -0.005938;
+	a[1][1] = 1.175941;
+	a[1][2] = -0.000828;
+	a[2][2] = 1.178647;
+	a[1][0] = a[0][1];
+	a[2][0] = a[0][2];
+	a[2][1] = a[1][2];
+	b[0] = 4.928107;
+	b[1] = -4.679924;
+	b[2] = 2.395619;
+
+	//вывод калиброванных данных магнитометра
+	result[0] = a[0][0] * (dataMagnet[0] - b[0]) + a[0][1] * (dataMagnet[1] - b[1]) + a[0][2] * (dataMagnet[2] - b[2]);
+	result[1] = a[1][0] * (dataMagnet[0] - b[0]) + a[1][1] * (dataMagnet[1] - b[1]) + a[1][2] * (dataMagnet[2] - b[2]);
+	result[2] = a[2][0] * (dataMagnet[0] - b[0]) + a[2][1] * (dataMagnet[1] - b[1]) + a[2][2] * (dataMagnet[2] - b[2]);
 }
