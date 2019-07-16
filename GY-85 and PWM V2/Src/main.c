5236377ace99472eaec9fe3cbe9f49d0915a722e
Ilor5 <48730197+Ilor5@users.noreply.github.com>
2019-07-16 12:41:17 +0300

    fixed bug with gyo saturation and commented part of main code with calculations for PWM

--- GY-85 and PWM V2/Src/main.c
+++ GY-85 and PWM V2/Src/main.c
@@ -60,8 +60,8 @@ float currentAngle;		 /*Текущий угол*/
 int pinStatus = 0;		 /*Статус цифровых пинов для ШИМ */
 float koeff = 321037;	/*Коэффициент гашения угловой скорости, нужно установить значение*/
 
-uint8_t PWM_PA3 = 240; /*Переменная со значением ШИМ на PA3 */
-uint8_t PWM_PA1 = 240; /*Переменная со значением ШИМ на PA1 */
+uint8_t PWM_PA3 = 0; /*Переменная со значением ШИМ на PA3 */
+uint8_t PWM_PA1 = 200; /*Переменная со значением ШИМ на PA1 */
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -138,20 +138,20 @@ int main(void) {
 		/* USER CODE BEGIN 3 */
 
 		/*Запись значений ШИМ, выставление значений ШИМ и включение катушек */
-		pwmValue(koeff, calibDataGyro[2], calibDataMagnet[1], -1, &PWM_PA3, &pinStatus);
+		/*pwmValue(koeff, calibDataGyro[2], calibDataMagnet[1], -1, &PWM_PA3, &pinStatus);
 		if (pinStatus == 0) {
 			HAL_GPIO_WritePin(GPIOB, IN_2_Pin, GPIO_PIN_RESET);
 		} else {
 			HAL_GPIO_WritePin(GPIOB, IN_2_Pin, GPIO_PIN_SET);
-		}
+		}*/
 		htim2.Instance->CCR4 = PWM_PA3;
 
-		pwmValue(koeff, calibDataGyro[2], calibDataMagnet[0], 1, &PWM_PA1, &pinStatus);
+		/*pwmValue(koeff, calibDataGyro[2], calibDataMagnet[0], 1, &PWM_PA1, &pinStatus);
 		if (pinStatus == 0) {
 			HAL_GPIO_WritePin(GPIOB, IN_4_Pin, GPIO_PIN_RESET);
 		} else {
 			HAL_GPIO_WritePin(GPIOB, IN_4_Pin, GPIO_PIN_SET);
-		}
+		}*/
 		htim2.Instance->CCR2 = PWM_PA1;
 		/*Выключение катушек ************************************************** */
 		HAL_Delay(100);
--- ITG3205/ITG3205.c
+++ ITG3205/ITG3205.c
@@ -3,33 +3,34 @@
 
 #define GYRO 0x68 //  когда AD0 подключен к GND, адресс гироскопа 0x68
 //#define GYRO 0x69   когда AD0 подключен к GND, адресс гироскопа 0x69
+#define GYRO_ADDR 0x1D
 #define G_SMPLRT_DIV 0x15
 #define G_DLPF_FS 0x16
 #define G_INT_CFG 0x17
 #define G_PWR_MGM 0x3E
-#define G_TO_READ 8 // по 2 байта на каждую ось x, y, z
+#define G_TO_READ 6 // по 2 байта на каждую ось x, y, z
 /* Смещение (менять не нужно) ******/
-int g_offx = 0; /* Смещение по x */
-int g_offy = 0; /* Смещение по y */
-int g_offz = 0; /* Смещение по z */
+float g_offx = 0; /* Смещение по x */
+float g_offy = 0; /* Смещение по y */
+float g_offz = 0; /* Смещение по z */
 
 /* Буфера данных (менять не нужно) *********************/
 byte buff[G_TO_READ]; /* Буфер сырых данных гироскопа */
-float GyroCal[4];		  /* Буфер для калибровки *********/
-
+float GyroCal[3];		  /* Буфер для калибровки *********/
+float rawDataGyro[3];
 // Калибровка ************************************************************************************************/
 void GyroCalibrate() {
 	/* Переменные временного смещения *****************/
-	static int tmpx = 0; /* временное смещения по x*/
-	static int tmpy = 0; /* временное смещения по y*/
-	static int tmpz = 0; /* временное смещения по z*/
+	static float tmpx = 0; /* временное смещения по x*/
+	static float tmpy = 0; /* временное смещения по y*/
+	static float tmpz = 0; /* временное смещения по z*/
 
 	/* Определние смещения */
 	for (uint8_t i = 0; i < 10; i++) /* Берем среднее значение с 10 считываний */
 	{								 /* */
-		HAL_Delay(10);				 /* */
-		getGyroscopeData(GyroCal);   /* */
-		tmpx += GyroCal[0];			 /* */
+		HAL_Delay(10);
+                getGyroscopeData(GyroCal);
+             	tmpx += GyroCal[0];			 /* */
 		tmpy += GyroCal[1];			 /* */
 		tmpz += GyroCal[2];			 /* */
 	}								 /* */
@@ -72,17 +73,20 @@ void getGyroscopeData(float *result) {
 	y axis MSB = 1F, y axis LSB = 20
 	z axis MSB = 21, z axis LSB = 22
 	**************************************/
-	/* Адрес регистра (менять не нужно) */
-	int regAddress = 0x1D;
-	int rawDataGyro[3];
 
-	I2Cdev_readBytes(GYRO, regAddress, 6, buff, 0);
+	
+	I2Cdev_readBytes(GYRO, GYRO_ADDR, 6, buff, 0);
 
-	rawDataGyro[0] = ((buff[0] << 8) + buff[1]) - g_offx;
-	rawDataGyro[1] = ((buff[2] << 8) + buff[3]) - g_offy;
-	rawDataGyro[2] = ((buff[4] << 8) + buff[5]) - g_offz;
-	result[0] = rawDataGyro[0] / 14.375; /* Скорость вращения по x*/
-	result[1] = rawDataGyro[1] / 14.375; /* Скорость вращения по y*/
-	result[2] = rawDataGyro[2] / 14.375; /* Скорость вращения по z*/
+	rawDataGyro[0] = ((buff[0] << 8) + buff[1]);
+	rawDataGyro[1] = ((buff[2] << 8) + buff[3]);
+	rawDataGyro[2] = ((buff[4] << 8) + buff[5]);
+        for (int i = 0; i < 3; i++) {
+          if (rawDataGyro[i] > 32768) {
+		rawDataGyro[i] = rawDataGyro[i] - 65536;		//проверка на переполнение
+          }
+	}
+	result[0] = rawDataGyro[0] / 14.375  - g_offx; /* Скорость вращения по x*/
+	result[1] = rawDataGyro[1] / 14.375  - g_offy; /* Скорость вращения по y*/
+	result[2] = rawDataGyro[2] / 14.375  - g_offz; /* Скорость вращения по z*/
 }
 /**********************************************/
